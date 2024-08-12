6599f769e0baf92fb6ef0c06163f4ce557a6351c
Lepha <lepharamchiary@gmail.com>
2024-08-12 10:47:47 +0530

    basic mongodb connection using iife function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hweqhjfjkhfc");
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express";
+import 'dotenv/config';
+
+const app = express()
+const port = process.env.PORT;
+const mongoDB_URL = process.env.MONGODB_URL;
+
+// Immediately-invoked function expression (IIFE)
+;( async () => {
+    try {
+        await mongoose.connect(`${mongoDB_URL}/${DB_NAME}`)
+        console.log("db connected to mongo")
+        app.on("error", (error) => {
+            console.log("db not connect to mongo", error)
+            throw error
+        })
+
+        app.listen(port, () => {
+            console.log(`Server is live on port: ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+})()
