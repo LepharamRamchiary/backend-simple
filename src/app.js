8e1045bf7938ef4779239dd2b17f9eea0930b7ab
Lepha <lepharamchiary@gmail.com>
2024-08-19 10:00:57 +0530

    adding a simple route and controller for user

--- src/app.js
+++ src/app.js
@@ -17,5 +17,11 @@ app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users", userRouter) 
+
 
 export { app }
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message = "Something going to wrong!!",
         errors = [],
-        statck = ""
+        stack = ""
     ) {
         super(message)
         this.statusCode = statusCode
