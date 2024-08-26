7a12b8d0413cc1563745b9a9557ab40c9fe9c421
Lepha <lepharamchiary@gmail.com>
2024-08-26 11:46:48 +0530

    complet loggout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,8 +194,30 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    // find user
-    
+    // removing refreshToken from DB 
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    // clearing a cookies 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out Successfully"))
 })
 
 export { registerUser, loginUser, logoutUser }
