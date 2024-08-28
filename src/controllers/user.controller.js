e9dce0b6cd83a188c4004c865999bb1e91ba0fff
Lepha <lepharamchiary@gmail.com>
2024-08-28 11:51:02 +0530

    bug fixing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-import jet from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 
 
 //generate access and referesh token
