0964edbcf64f664f26be35cfd0432dc7ff7622f4
Lepha <lepharamchiary@gmail.com>
2024-09-04 11:25:55 +0530

    fixing bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -201,7 +201,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         req.user._id,
         {
             $set: {
-                refreshToken: undefined
+                refreshToken: 1
             }
         },
         {
