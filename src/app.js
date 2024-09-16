dce4073ae253fcf0730d4110a24884552f074ca7
Lepha <lepharamchiary@gmail.com>
2024-09-16 09:14:04 +0530

    add all routers and video like controller

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import likeRouter from "./routes/like.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter) 
@@ -30,6 +31,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/likes", likeRouter)
 
 
 export { app }
---
+++ src/controllers/like.controller.js
@@ -0,0 +1,63 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+//helper function for like 
+const createLike = async (userId, targetId, targetType) => {
+    const existingLike = await Like.findOne({
+        [targetType]: targetId,
+        likedBy: userId
+    })
+
+    if (existingLike) {
+        await existingLike.romove()
+        return { message: "Like removed", action: "dislike" }
+    } else {
+        const newLike = new Like({
+            [targetType]: targetId,
+            likedBy: userId
+        })
+        await newLike.save()
+        return { message: "Like Successfully", action: "like" }
+    }
+}
+
+const toggleVideoLike = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+    //TODO: toggle like on video
+    const userId = req.user._id
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const result = await createLike(userId, videoId, "video")
+    return res
+        .status(200)
+        .json(new ApiResponse(200, result, "Like successfully"))
+})
+
+const toggleCommentLike = asyncHandler(async (req, res) => {
+    const { commentId } = req.params
+    //TODO: toggle like on comment
+
+})
+
+const toggleTweetLike = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params
+    //TODO: toggle like on tweet
+}
+)
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+    //TODO: get all liked videos
+})
+
+export {
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
