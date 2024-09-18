56c3011d8211968887c745322229286f226a99cc
Lepha <lepharamchiary@gmail.com>
2024-09-18 09:21:17 +0530

    create routes for play list

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter) 
@@ -32,6 +33,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
 
 
 export { app }
