9e12b66a6b18057287b679bdd8cba3a052af6ef1
Lepha <lepharamchiary@gmail.com>
2024-09-19 11:52:46 +0530

    add routers for deshboard

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter) 
@@ -34,6 +35,7 @@ app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 export { app }
