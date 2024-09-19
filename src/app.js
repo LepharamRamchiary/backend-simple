eb1160cd2161aafbbceedb8f6d5f609135b2e49c
Lepha <lepharamchiary@gmail.com>
2024-09-19 12:32:57 +0530

    add router and controller for healt check

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter) 
@@ -36,6 +37,7 @@ app.use("/api/v1/subscriptions", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
 export { app }
